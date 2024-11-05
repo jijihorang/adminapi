29c9eb5116ded455e74dfb9ceae5943dd51654ad
Honggeun Lee <kwy05115@gmail.com>
2024-11-05 16:04:26 +0900

    Initial commit

---
+++ src/test/java/org/example/adminapi/AdminapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.adminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
