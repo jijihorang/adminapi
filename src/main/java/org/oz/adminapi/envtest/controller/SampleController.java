c68543384afa4d9c6de086f9e75cc9ae287a5c2a
MinJae Kim <skwze@naver.com>
2024-11-06 14:15:24 +0900

    HQT70 KMJ Init Setting (2024-11-06) Kafka 설정 주석 처리 ex삭제 MakerEntity 컬럼 설정 AttachFile ElementCollection 설정

--- src/main/java/org/oz/adminapi/envtest/controller/SampleController.java
+++ src/main/java/org/oz/adminapi/envtest/controller/SampleController.java
@@ -1,36 +1,36 @@
-package org.oz.adminapi.envtest.controller;
-
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.CompletableFuture;
-
-@RestController
-@RequiredArgsConstructor
-@Log4j2
-public class SampleController {
-
-    private final KafkaTemplate<String, Object> kafkaTemplate;
-
-    @Value("${org.oz.v1}")
-    private String v1;
-
-    @Value("${org.oz.v2}")
-    private String v2;
-
-    @GetMapping("hello")
-    public String[] sayHello(){
-
-        CompletableFuture<SendResult<String, Object>> result = kafkaTemplate.send("topic", "say hello : " + System.currentTimeMillis());
-
-
-
-        return new String[]{"AAA","BBB","CCC"};
-    }
-}
+//package org.oz.adminapi.envtest.controller;
+//
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.log4j.Log4j2;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.kafka.support.SendResult;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.concurrent.CompletableFuture;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@Log4j2
+//public class SampleController {
+//
+//    private final KafkaTemplate<String, Object> kafkaTemplate;
+//
+//    @Value("${org.oz.v1}")
+//    private String v1;
+//
+//    @Value("${org.oz.v2}")
+//    private String v2;
+//
+//    @GetMapping("hello")
+//    public String[] sayHello(){
+//
+//        CompletableFuture<SendResult<String, Object>> result = kafkaTemplate.send("topic", "say hello : " + System.currentTimeMillis());
+//
+//
+//
+//        return new String[]{"AAA","BBB","CCC"};
+//    }
+//}
--- src/main/java/org/oz/adminapi/envtest/listener/MsgConsumer.java
+++ src/main/java/org/oz/adminapi/envtest/listener/MsgConsumer.java
@@ -1,28 +1,28 @@
-package org.oz.adminapi.envtest.listener;
-
-
-import lombok.extern.log4j.Log4j2;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
-
-@Component
-@Log4j2
-public class MsgConsumer {
-
-    @KafkaListener(topics = "topic")
-    public void listener(Object data) {
-
-
-        log.info("2---------------------------------------------------");
-
-        log.info("2---------------------------------------------------");
-
-        log.info("2---------------------------------------------------");
-
-        log.info("2---------------------------------------------------");
-
-        log.info("2---------------------------------------------------");
-
-        System.out.println(data);
-    }
-}
+//package org.oz.adminapi.envtest.listener;
+//
+//
+//import lombok.extern.log4j.Log4j2;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Log4j2
+//public class MsgConsumer {
+//
+//    @KafkaListener(topics = "topic")
+//    public void listener(Object data) {
+//
+//
+//        log.info("2---------------------------------------------------");
+//
+//        log.info("2---------------------------------------------------");
+//
+//        log.info("2---------------------------------------------------");
+//
+//        log.info("2---------------------------------------------------");
+//
+//        log.info("2---------------------------------------------------");
+//
+//        System.out.println(data);
+//    }
+//}
